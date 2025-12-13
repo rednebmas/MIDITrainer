449d765119013fcde6e757f003c6a86f8aa587e9
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2025-12-12 17:19:16 -0800

    Initial Commit

---
+++ MIDITrainer/MIDITrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDITrainerApp.swift
+//  MIDITrainer
+//
+//  Created by Sam Bender on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MIDITrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
